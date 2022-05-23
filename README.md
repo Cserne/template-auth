190aec3242ea4fa3ea271fff6d94e41c1f68680e
Cserne <angeli.cserne@gmail.com>
2022-05-23 11:12:52 +0200

    Middlewares added.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# template-auth
+# todo api
+
+## system requirements
+- nodejs
+- npm
+
+### environment variables
+Create .env file with the following variables:
+- PORT=[4000]
+- APP_URL=http://localhost:3000
+
+## dev start
+
+- npm install
+- npm start
+
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,42 @@
 require("dotenv").config();
-// const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors());
-app.use(express.json());
+const corsOptions = {
+    origin: process.env.APP_URL,
+    optionsSuccessStatus: 200,
+};
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const myLogger = (req, res, next) => {
+    console.log('Épp logolok...');
+    next(); //ezzel hívom meg a következő middleware functiont (a myAuth-ot);
+};
+
+const myAuth = (req, res, next) => {
+    console.log('Épp autentikálok...');
+    const userId = 12;
+    res.locals.userId = userId;
+    next();
+};
+
+const myBusinessLogic = (req, res) => {
+    if (!res.locals.userId) return res.sendStatus(401);
+    console.log('Épp fut a logikám...');
+    res.status(200).json('Oké');
+}
+
+app.use(myLogger);
+app.use(myAuth);
+app.use(myBusinessLogic);
+// app.use(cors(corsOptions));
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
